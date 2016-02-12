6e89789ea3f2b5f280f2ba778994b5629013c4bb
renatosalas <juventus_s12@live.com.mx>
2016-02-12 16:10:06 -0600

    UI: center google maps & custom marker

--- app/src/main/java/com/example/andrestorresb/ccar/MainActivity.java
+++ app/src/main/java/com/example/andrestorresb/ccar/MainActivity.java
@@ -1,19 +1,17 @@
 package com.example.andrestorresb.ccar;
 
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -24,8 +22,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     GoogleMap map;
     Boolean statusProtection = false;
 
-    final String PROTECTION_ON = "Protegido";
-    final String PROTECTION_OFF = "No Protegido";
+    final String PROTECTION_ON = "Protegido",
+                 PROTECTION_OFF = "No Protegido",
+                 LOCALIZATION = "Localizando...";
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,7 +45,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
         //Generate google map
         MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.mapa);
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
     }
@@ -54,6 +53,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void protect(View v){
         //Not Protected; therefore protect
         if(!this.statusProtection){
+            //Notify user change
+            this.ShowToast(PROTECTION_ON, Toast.LENGTH_SHORT);
+
             //Change status label
             this.statusProtectionLabel.setText(PROTECTION_ON);
 
@@ -66,6 +68,9 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }else{
             //Protected; therefore unprotect
 
+            //Notify user change
+            this.ShowToast(PROTECTION_OFF, Toast.LENGTH_SHORT);
+
             //Change status label
             this.statusProtectionLabel.setText(PROTECTION_OFF);
 
@@ -77,23 +82,40 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    public void localizado(View v) {
-        /*
-        this.txtV.setText("Localizado");
-        this.txtV2.setText("420 m de ti");
-        this.fab.hide();
-        Intent i = new Intent(this, Alerta.class);
-        startActivity(i);
-        */
-        //Would be better to make a another activity for localization
-        Toast.makeText(this, "Localizando...", Toast.LENGTH_SHORT).show();
-        this.localizationButton.setBackgroundResource(R.drawable.localization_on);
+    public void locate(View v) {
+        //Notify user change
+        this.ShowToast(LOCALIZATION, Toast.LENGTH_SHORT);
+
+        //Set position of car (from CCAR Platform)
+        this.carLocate();
     }
 
     @Override
     public void onMapReady(GoogleMap map) {
-        map.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+        //Set position of car (from CCAR Platform)
+        this.map = map;
+        this.carLocate();
+    }
+
+    //Generate notification
+    private void ShowToast(String msg, int duration){
+        Toast.makeText(this, msg, duration).show();
+    }
+
+    //Set position of car (from CCAR Platform)
+    private void carLocate(){
+        //Get position from CCAR Platform
+        double lat = 20.735014,
+                lon = -103.456280;
+        float zoom = 15f; //Street level
+
+        //Center map
+        this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), zoom));
+
+        //Add custom marker
+        this.map.addMarker(new MarkerOptions()
+                .position(new LatLng(lat, lon))
+                .title("My Car")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.pin_car_location)));
     }
 }
