d5e40ab782347b495cb0375723b372701f5949f2
renatosalas <juventus_s12@live.com.mx>
2016-02-13 12:56:56 -0600

    UI: localization CCAR Platform

--- app/src/main/java/com/example/andrestorresb/ccar/MainActivity.java
+++ app/src/main/java/com/example/andrestorresb/ccar/MainActivity.java
@@ -7,25 +7,56 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.client.AuthData;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback, Firebase.AuthResultHandler, ValueEventListener{
 
-    ImageButton protectionButton, localizationButton;
-    TextView statusLabel,txtV2;
-    GoogleMap map;
-    Boolean statusProtection = false,
-            statusLocalization = false;
+    private ImageButton protectionButton, localizationButton;
+    private TextView statusLabel,txtV2;
+    private GoogleMap map;
+    private Boolean statusProtection = false,
+                    statusLocalization = false;
 
-    final String PROTECTION_ON = "Protegido",
-                 PROTECTION_OFF = "No Protegido",
-                 LOCALIZATION = "Localizando";
+    private final String PROTECTION_ON = "Protegido",
+                         PROTECTION_OFF = "No Protegido",
+                         LOCALIZATION = "Localizando";
+
+    //Google maps
+    private Marker currentMarker,
+                   prevMarker;
+    private boolean initialPosition = true;
+
+    //Firebase credentials
+    private Firebase fb;
+    private final String FIREBASE_URL = "https://ccar-app.firebaseio.com/";
+    private final String FIREBASE_TOKEN = "EIiCIYN5Ef7WXqr2VTdwBYNF1uZQ0HMtx73YhoeP";
+
+    //Firebase status
+    private int actionFirebase = 0;
+    private final int GET_CAR_LOCATION = 0;
+
+    //CCAR Platform paths
+    private final String DEVICES = "monitor/devices";
+    private final String USERS = "monitor/users/";
+
+    //Device
+    private double lat = 0.0,
+                   lon = 0.0;
+    private int timeLastLocation = 0;
+
+
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,11 +75,13 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         this.statusLabel = (TextView)findViewById(R.id.statusLabel);
         this.txtV2 = (TextView)findViewById(R.id.metros);
 
+        //Initialize Firebase
+        this.initializeFirebase();
+
         //Generate google map
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
     public void protect(View v){
@@ -84,6 +117,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void locate(View v) {
+
         //Locate Car
         if(!this.statusLocalization){
             //Hide protectionButton
@@ -101,8 +135,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             //Notify user change
             this.ShowToast(LOCALIZATION, Toast.LENGTH_SHORT);
 
-            //Set position of car (from CCAR Platform)
-            this.carLocate();
+            //Get & Set car location from CCAR Platform
+            this.getDeviceLocation();
 
         }else{
             //Exit from localization
@@ -140,6 +174,11 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap map) {
         //Set position of car (from CCAR Platform)
         this.map = map;
+
+        //Get car location from CCAR Platform
+        this.getDeviceLocation();
+
+        //Set car location from CCAR Platform
         this.carLocate();
     }
 
@@ -150,18 +189,81 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
 
     //Set position of car (from CCAR Platform)
     private void carLocate(){
-        //Get position from CCAR Platform
-        double lat = 20.735014,
-                lon = -103.456280;
         float zoom = 15f; //Street level
 
         //Center map
-        this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lon), zoom));
+        this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(this.lat, this.lon), zoom));
+
+        //Remove previous markers; only on screen current marker
+        if(this.prevMarker != null) this.prevMarker.remove();
 
         //Add custom marker
-        this.map.addMarker(new MarkerOptions()
-                .position(new LatLng(lat, lon))
-                .title("My Car")
-                .icon(BitmapDescriptorFactory.fromResource(R.drawable.pin_car_location)));
+        this.currentMarker = this.map.addMarker(new MarkerOptions()
+                                    .position(new LatLng(this.lat, this.lon))
+                                    .title("My Car")
+                                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.pin_car_location)));
+
+        this.prevMarker = this.currentMarker;
+    }
+
+    //Initialize Firebase
+    private void initializeFirebase(){
+        //Initialize Firebase
+        Firebase.setAndroidContext(this);
+        this.fb = new Firebase(FIREBASE_URL);
+
+        //Auth in Firebase
+        this.fb.authWithCustomToken(FIREBASE_TOKEN, this);
+    }
+
+    //Retrieve Device Location from CCAR Platform
+    private void getDeviceLocation(){
+        this.actionFirebase = GET_CAR_LOCATION;
+
+        //Get location from CCAR Platform
+        String deviceID = "renato";
+        this.fb.child(DEVICES + "/" + deviceID + "/location").addValueEventListener(this);
+    }
+
+    //Firebase callback on Auth
+    @Override
+    public void onAuthenticated(AuthData authData) {
+        //this.ShowToast("Auth OK", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onAuthenticationError(FirebaseError firebaseError) { this.ShowToast("Fail to Auth", Toast.LENGTH_LONG); }
+
+    //Firebase callback on SingleEvent
+    @Override
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        if(dataSnapshot == null) return;
+
+        //Detect action request to Firebase
+        switch (this.actionFirebase){
+            case GET_CAR_LOCATION:
+                //Set location of device
+                this.lat = Double.parseDouble(dataSnapshot.child("lat").getValue().toString());
+                this.lon = Double.parseDouble(dataSnapshot.child("lon").getValue().toString());
+                this.timeLastLocation = Integer.parseInt(dataSnapshot.child("time").getValue().toString());
+
+                //Represent in map
+
+                //When localization module is ON
+                if(this.statusLocalization) this.carLocate();
+
+                //When initialPosition is ON
+                if(this.initialPosition){
+                    this.carLocate();
+                    this.initialPosition = false;
+                }
+
+                break;
+        }
+    }
+
+    @Override
+    public void onCancelled(FirebaseError firebaseError) {
+
     }
 }
